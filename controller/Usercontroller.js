bf2bd5ae83e2cf5fb674458745f2c9d3877b477d
Hitanshu Shah <hitanshushah5@gmail.com>
2024-05-15 02:29:45 +0530

    Premium subscription removed for now

--- controller/Usercontroller.js
+++ controller/Usercontroller.js
@@ -1,25 +1,33 @@
 const multer = require("multer");
 const s3Storage = require("../Utils/S3ClientIntiate");
+const {
+  REQUESTS_CONTAINER,
+  PROFILE_PHOTO_CONTAINER,
+  SNEAKER_PHOTO_CONTAINER,
+  PROFILE_PHOTO,
+  REQUEST_PHOTO,
+  SNEAKER_PHOTOS,
+} = require("../Constants/constants");
 
 // our middleware
 const uploadRequestImage = multer({
-  storage: s3Storage("sneakertradesneakerrequests"),
+  storage: s3Storage(REQUESTS_CONTAINER),
   limits: {
     fileSize: 1024 * 1024 * 5, // 2mb file size
   },
 });
 
 const uploadProfileImage = multer({
-  storage: s3Storage("sneakertradesprofilephotos"),
+  storage: s3Storage(PROFILE_PHOTO_CONTAINER),
   limits: {
     fileSize: 1024 * 1024 * 5, // 2mb file size
   },
 });
 
 const uploadSneakerImage = multer({
-  storage: s3Storage("sneakertradessneakerphotos"),
+  storage: s3Storage(SNEAKER_PHOTO_CONTAINER),
 });
 
-exports.UserPhoto = uploadProfileImage.single("ProfilePhoto");
-exports.SneakerPhoto = uploadRequestImage.single("Photo");
-exports.SneakerPhotos = uploadSneakerImage.array("Photos");
+exports.UserPhoto = uploadProfileImage.single(PROFILE_PHOTO);
+exports.SneakerPhoto = uploadRequestImage.single(REQUEST_PHOTO);
+exports.SneakerPhotos = uploadSneakerImage.array(SNEAKER_PHOTOS);
